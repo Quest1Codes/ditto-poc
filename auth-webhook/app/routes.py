e277124b5b7f31c1d0c499ab57756a2f38da939c
surya-quest1 <suryak@quest1.io>
2025-07-29 12:00:16 +0530

    auth webhook chnge

--- auth-webhook/app/routes.py
+++ auth-webhook/app/routes.py
@@ -66,7 +66,7 @@ def auth_webhook():
 
         response = {
             "authenticated": True,
-            "userID": user_id,
+            "userID": "6880674ccd221c74c058ab73",
             "expirationSeconds": 8 * 60 * 60,
             "permissions": permissions,
             "identityServiceMetadata": {"userRole": role},
